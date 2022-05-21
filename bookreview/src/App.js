17e0a6649b6fb54cc474f9c522e870dfd89e72e6
oliverahyou <ahyouoliver9@gmail.com>
2022-05-21 17:40:05 +0400

    21 May 17:39

---
+++ bookreview/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Books from './components/Books';
+
+
+function App() {
+  return (
+    <div className="App">
+      type
+      <Books/>
+    </div>
+  );
+}
+
+export default App;
